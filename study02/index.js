03fae4ead740a8645c3d573eb890e8e5c675f6cd
imhonggeun <dkfl9870@gmail.com>
2025-07-14 10:49:31 +0900

    git html

--- study02/index.js
+++ study02/index.js
@@ -4,7 +4,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    var html = `<h1>화면 연습</h1>
+            <p>node.js 처음이지만 힘들지 않네!</p>`
+  res.send(html)
 })
 
 app.listen(port, () => {
