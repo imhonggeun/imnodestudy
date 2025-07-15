fd5e18695d0fbf51a3b85c35b070beb2cf819506
imhonggeun <dkfl9870@gmail.com>
2025-07-15 10:04:29 +0900

    Object 반복문 알아보기

---
+++ study04/app2.js
@@ -0,0 +1,16 @@
+a = {
+    "k1" : "v1",
+    "k2" : "v2",
+    "k3" : "v3",
+}
+
+for (let i in a){
+    console.log(a)
+    console.log(i) 
+    console.log(a[i])
+}
+
+// a = "ABCDEFG"
+// for (let i of a) {
+//     console.log(i)
+// }
