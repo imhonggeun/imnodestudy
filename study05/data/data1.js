e05942b1252bc39bc601198a5a213b19ab82cc6e
imhonggeun <dkfl9870@gmail.com>
2025-07-16 09:38:43 +0900

    stduy 05 css,data,app.js.indexhtml 가져오기

---
+++ study05/data/data1.js
@@ -0,0 +1,13 @@
+const model = [
+    [0,1,0,1,0],
+    [1,0,1,0,1],
+    [0,1,0,1,0],
+    [1,0,1,0,1],
+    [0,1,0,1,0]
+];
+
+// CommonJS 방식
+//module.exports = model;
+
+// ES6 방식
+//export default model;
