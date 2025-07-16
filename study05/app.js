aa2c2ce77539e4a8a1ef1346bbe2e8a4675c74e5
imhonggeun <dkfl9870@gmail.com>
2025-07-16 11:14:52 +0900

    화면 출력

--- study05/app.js
+++ study05/app.js
@@ -22,9 +22,11 @@ import model from "./data/data1.js";
 
 // 데이터 확인
 //console.log(model);
-function view(){
+//let data = model.model1; // data1.js 에서 받아옴
+
+function view(data) {
     let html ="";
-    for (const li of model){
+    for (const li of data) {
         let str ="";
         for(const div of li){
             str += `<div class="${div === 1 ? 'bg1' : 'bg2'}"></div>`;
@@ -34,3 +36,34 @@ function view(){
     document.getElementsByTagName("ul")[0].innerHTML = html;
 }
 
+function btn1() {
+    view(model.model1)
+}
+function btn2() {
+    view(model.model2)
+}
+function btn3() {
+    view(model.model3)
+}
+
+// const btn1 = document.getElementById("1"); //index.htmldp 있는 버튼에 있는 ID 값 1번을호출
+// btn1.addEventListener("click", view); //위에 view 함수를 적용한다
+
+// const btn2 = document.getElementById("2"); //index.htmldp 있는 버튼에 있는 ID 값 2번을호출
+// btn2.addEventListener("click", view); //위에 view 함수를 적용한다
+
+// const btns = document.getElementsByTagName("button");
+// for(const btn of btns) {
+//     btn.addEventListener("click", view);
+// }
+
+const btns = document.getElementsByTagName("button");
+for (let i = 0; i < btns.length; i++) {
+    if(i === 0) {
+        btns[i].addEventListener("click", btn1);
+    } else if(i === 1) {
+        btns[i].addEventListener("click", btn2);
+    } else if(i === 2) {
+        btns[i].addEventListener("click", btn3);
+    }
+}
